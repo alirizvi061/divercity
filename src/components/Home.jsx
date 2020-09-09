b4e2987ff89f420922f9e2a2cdd3350c79d8ed34
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-09-09 15:14:20 -0700

    tried working on the multiple filter feature but hitting blockades. Going to switch gears and work on sign up and log in features

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,14 +42,10 @@ class Home extends Component {
 
     renderSearch = (job, index) => {
         const search = this.state.search;
-        console.log(search)
 
-        if (search !== "" && job.title.toLowerCase().indexOf(search.toLowerCase()) === -1) {
-            return null
-        } else if (search !== "" && job.location.toLowerCase().indexOf(search.toLowerCase()) === -1) {
+        if (search !== " " && job.title.toLowerCase().indexOf(search.toLowerCase()) === -1) {
             return null
         }
-
 
         return <div className="job" key={index}>
             <p>Title: {job.title}</p>
@@ -65,28 +61,24 @@ class Home extends Component {
             <div>
                 <h1>Job Listing</h1> {/*This is the area where job information will be displayed */}
                 <form className="form">
+                    {/* <label for="key">Choose search type:</label>
+                    <select name="key" id="cars">
+                        <option value="title">Title</option>
+                        <option value="location">Location</option>
+                        <option value="skills">Skills</option> */}
+                    {/* </select> */}
                     <input type="text" name="search" placeholder="location, job type, skills..." onChange={this.handleChange} />
                     {/* <input type="text" placeholder="Job Type" />
                     <input type="text" placeholder="Skills" /> */}
-                    <input type="submit" />
+                    {/* <input type="submit" /> */}
                 </form>
                 <div className="jobDescription">
                     {this.state.jobs.map((job, index) => {
-                        return this.renderSearch(job, index)
-                        //     console.log(job)
-                        //     return (
-                        //         <div className="job" key={index}>
-                        //             <p>Title: {job.title}</p>
-                        //             <p>Company: {job.company}</p>
-                        //             <p>Location: {job.location}</p>
-                        //             <p>Description: {job.description}</p>
-                        //             <p>Skills: {job.skills_tag}</p>
-                        //         </div>
-                        //     )
+                        return this.renderSearch(job, index) /* This embedded JS calls the renderSearch function with the result from the jobs.map as a parameter */
                     })
                     }
                 </div>
-            </div>
+            </div >
         )
     }
 }
